e2107d36953946b150fb6bf370ebe2bddaa34134
juki3r <deepong25@gmail.com>
2025-09-05 12:03:42 +0800

    modified:   resources/views/auth/register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,6 @@
 <x-guest-layout>
+
+    <h3 class="fs-3 text-center w-full">Register</h3>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -39,14 +41,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="mt-4">
+            <x-primary-button class="py-3 text-center w-full" style="text-align:center !important;">
+              {{ __('Register') }}
+            </x-primary-button>
+        </div>
+
+        <div class="flex items-center justify-start mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
