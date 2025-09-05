d2a619eb25dd9355f1a4a4a3a6253424043ec2e3
juki3r <deepong25@gmail.com>
2025-09-05 11:20:04 +0800

    modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="bg-white border-b shadow border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-2 lg:px-8">
+    <div class="max-w-7xl mx-auto px-2 sm:px-2 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
