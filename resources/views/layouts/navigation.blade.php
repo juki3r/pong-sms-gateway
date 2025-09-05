22b19d372a719e72f3874de3f8384a725ef56305
juki3r <deepong25@gmail.com>
2025-09-05 11:09:23 +0800

    modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                         <img src="{{ asset('logo.png') }}" alt="" width="100px">
+                         <img src="{{ asset('logo.png') }}" alt="" width="80px">
                     </a>
                 </div>
 
