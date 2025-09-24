321a8bc63cac1d017664a84b3a44b895cf14332b
juki3r <deepong25@gmail.com>
2025-09-24 11:15:05 +0800

    new file:   app/Http/Controllers/AuthController.php 	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -3,18 +3,39 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SmsGatewayController;
+use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/send-sms', [SmsGatewayController::class, 'sendSms']);
 });
 
 Route::post('/send-sms-api', [SmsGatewayController::class, 'sendSmsAPI']);
-
-
-
 Route::get('/fetch-sms', [SmsGatewayController::class, 'fetchSms']);
 Route::post('/update-sms-status', [SmsGatewayController::class, 'updateSmsStatus']);
 Route::post('/ping', [SmsGatewayController::class, 'ping']);
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/email/resend', [AuthController::class, 'resendVerificationEmail']);
+});
+
+Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])
+    ->middleware('signed')
+    ->name('verification.verify');
+
+
+
+
+
+
+
+
+
+
+
 
 
 Route::get('/test-api', function () {
