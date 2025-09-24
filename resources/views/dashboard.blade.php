65c36f611b70fb23eb8ccffeca30dab22546abf1
juki3r <deepong25@gmail.com>
2025-09-24 20:13:14 +0800

    aa

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -71,9 +71,11 @@
                             </div>
                             @else
                             <div class="text-danger d-flex justify-content-center align-items-center gap-3 rounded p-3">
-                                <p>
-                                    Not enough credits, Please rent sim card now to continue and unlocked more features.
-                                </p>
+                                <div class="col">
+                                    <p>
+                                        Not enough credits, Please rent sim card now to continue and unlocked more features.
+                                    </p>
+                                </div>
                             </div>
                             @endif
                         </div>
