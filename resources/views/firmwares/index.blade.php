32b41122711b274118317a72274d6510abb895e4
juki3r <deepong25@gmail.com>
2025-09-25 16:40:17 +0800

    aaa

--- resources/views/firmwares/index.blade.php
+++ resources/views/firmwares/index.blade.php
@@ -40,7 +40,7 @@
                                         <td>{{ $firmware->ota_key }}</td>
                                         <td>{{ $firmware->file_path }}</td>
                                         <td>
-                                            <a href="{{ route('firmwares.edit', $firmware->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                                            <a href="" class="btn btn-sm btn-primary">Edit</a>
                                             <form action="{{ route('firmwares.destroy', $firmware->id) }}" method="POST" style="display:inline-block;">
                                                 @csrf
                                                 @method('DELETE')
